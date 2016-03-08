7cced15dabf235b24d032891ca5587a485b45a9d
pellenberger <p.ellenberger@outlook.com>
2016-03-08 21:40:14 +0100

    ping script : add loop/delay + exception handling

--- conf.py
+++ conf.py
@@ -4,6 +4,9 @@
 INTERFACE='eth0'
 FIREBASE_URL='https://pi-temp.firebaseio.com/'	
 
+# All delays are in seconds
+DELAY_PING=5
+
 # Configure the logging module to write all messages to /log/<filename> with a specific text format
 def configure_log(logging, filename): 
 	logging.basicConfig(filename='log/' + filename, format='%(levelname)s:%(asctime)s *** %(message)s', datefmt='%d-%m-%Y %H:%M:%S', level=logging.DEBUG)
--- pi-temp-ping.py
+++ pi-temp-ping.py
@@ -6,22 +6,36 @@ import subprocess
 import time
 import json
 import requests
+import sys
 
-from conf import INTERFACE, FIREBASE_URL, configure_log
+from conf import INTERFACE, FIREBASE_URL, DELAY_PING, configure_log
 
 def get_ip_address(): 
-	ifconfig = subprocess.Popen(['ifconfig', INTERFACE], stdout=subprocess.PIPE)
-	output = subprocess.check_output(('grep', 'inet a'), stdin=ifconfig.stdout)
-	return output.split()[1].split(':')[1]
+	ip = ''
+	try: 
+		ifconfig = subprocess.Popen(['ifconfig', INTERFACE], stdout=subprocess.PIPE)
+		output = subprocess.check_output(('grep', 'inet a'), stdin=ifconfig.stdout)
+		ip = output.split()[1].split(':')[1]
+	except:
+		logging.error('Error when parsing ip address')
+		pass		
+	return ip
 
 def get_current_time():
 	return time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(time.time())) 
 
 if __name__ == '__main__':
-	configure_log(logging, 'pi-temp-ping.log')
-	last_ping = json.dumps({'ip':get_ip_address(), 'time':get_current_time(), 'delay':'fake'})
-	response = requests.put(FIREBASE_URL + 'last-ping.json', last_ping)
-	print response	
+
+	while True:
+		configure_log(logging, 'pi-temp-ping.log')
+		last_ping = json.dumps({'ip':get_ip_address(), 'datetime':get_current_time(), 'delay':DELAY_PING})
+		try:
+			requests.put(FIREBASE_URL + 'lastPing.json', last_ping)		
+		except:
+			logging.error('Error when executing firebase request')
+			pass
+
+		time.sleep(DELAY_PING)	
 
 
 
