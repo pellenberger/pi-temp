b1d427aad96526e06929ba42cf31c51e1ec519ca
pellenberger <p.ellenberger@outlook.com>
2016-03-10 17:42:58 +0100

    Fix log folder path

--- conf.py
+++ conf.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2.7
 #-*- coding: utf-8 -*-
 
+import sys, os
+
 INTERFACE='eth0'
 FIREBASE_URL='https://pi-temp.firebaseio.com/'	
 
@@ -9,4 +11,9 @@ DELAY_PING=5
 
 # Configure the logging module to write all messages to /log/<filename> with a specific text format
 def configure_log(logging, filename): 
-	logging.basicConfig(filename='log/' + filename, format='%(levelname)s:%(asctime)s *** %(message)s', datefmt='%d-%m-%Y %H:%M:%S', level=logging.DEBUG)
+	logging.basicConfig(filename=get_abs_path() + '/log/' + filename, format='%(levelname)s:%(asctime)s *** %(message)s', datefmt='%d-%m-%Y %H:%M:%S', level=logging.DEBUG)
+
+def get_abs_path(): 
+	pathname = os.path.dirname(sys.argv[0])
+	return os.path.abspath(pathname)
+	
