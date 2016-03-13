202a8a558ad3e6a8e3a08e1f8186c1c0a65288fc
pellenberger <p.ellenberger@outlook.com>
2016-03-13 18:53:26 +0100

    Add measure + push delays to last ping informations

--- pi-temp-ping.py
+++ pi-temp-ping.py
@@ -8,7 +8,7 @@ import json
 import requests
 import sys
 
-from conf import INTERFACE, FIREBASE_URL, DELAY_PING
+from conf import INTERFACE, FIREBASE_URL, DELAY_PING, DELAY_MEASURE, DELAY_PUSH
 from utils import configure_log, get_current_time
 
 def get_ip_address(): 
@@ -27,10 +27,10 @@ if __name__ == '__main__':
 	configure_log(logging, 'pi-temp-ping.log')
 
 	while True:		
-		last_ping = json.dumps({'ip':get_ip_address(), 'datetime':get_current_time(), 'delay':DELAY_PING})
+		last_ping = json.dumps({'ip':get_ip_address(), 'datetime':get_current_time(), 'delayPing':DELAY_PING, 'delayMeasure':DELAY_MEASURE, 'delayPush':DELAY_PUSH})
 
 		try:
-			requests.put(FIREBASE_URL + 'lastPing.json', last_ping)		
+			requests.put(FIREBASE_URL + 'lastPing.json', last_ping)					
 		except:
 			logging.error('Error when executing firebase request')
 			pass
